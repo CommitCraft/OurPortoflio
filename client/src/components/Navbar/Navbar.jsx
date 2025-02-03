81ac1ed03ed6ccac79d3eec63950840b17220f8b
kushwahavipin <kushwahavipin786@gmail.com>
2025-02-03 22:16:15 +0530

    update

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <>
+     
+      <div className="n-wrapper">
+      <div className="n-left">
+        <div className="n-name">Upskill Mafia</div>
+      </div>
+      <div className="n-right">
+        <div className="n-list">
+            <ul style={{listStyleType: "none"}}>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+        <button className="button n-button">Resume Download</button>
+      </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
