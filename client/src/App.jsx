697732e41daa26c61d9941c216069be64f3c6c21
kushwahavipin <kushwahavipin786@gmail.com>
2025-02-03 22:23:24 +0530

    intro compo add

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Intro from "./components/Intro/Intro";
 function App() {
   return (
     <div>
       <Navbar />
-     
+      <Intro />
     </div>
   );
 }
