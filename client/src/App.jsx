fe4c6601bb36068082931853dd4b18be0b8717f4
MansiP10 <mansi.prajapati101998@gmail.com>
2025-02-04 20:33:59 +0530

    assets and footer added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Intro from "./components/Intro/Intro";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div>
       <Navbar />
       <Intro />
+      <Footer />
     </div>
   );
 }
